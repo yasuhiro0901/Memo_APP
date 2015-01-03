0e9caaa8354c0d22267325c1e2cd41bb3ad9c92f
koji <yasuhirokojimakoyonplete@gmail.com>
2015-01-03 16:40:55 +0900

    入力画面での戻る、決定ボタンの処理を追加。

--- src/com/example/mymemo/InputActivity.java
+++ src/com/example/mymemo/InputActivity.java
@@ -2,14 +2,50 @@ package com.example.mymemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 //import android.view.Menu;
 //import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class InputActivity extends Activity {
-
+public class InputActivity extends Activity implements OnClickListener{
+	private EditText edit_title,edit_main;
+	private DataSingleton dataS;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_input);
+		setContentView(R.layout.activity_input_new);
+		 dataS = DataSingleton.getInstance();
+		
+		 ImageButton return_button = (ImageButton)findViewById(R.id.imageButton1);
+		 return_button.setOnClickListener(this);
+		 Button do_button = (Button)findViewById(R.id.button1);
+		 do_button.setOnClickListener(this);
+		 
+		 edit_title = (EditText)findViewById(R.id.editText1);
+		 edit_main = (EditText)findViewById(R.id.editText2);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.imageButton1://戻るボタン
+			finish();
+			break;
+		case R.id.button1:
+			boolean isAdd = false;
+			isAdd = dataS.AddData(edit_title.getText().toString(), edit_main.getText().toString());
+			if(!isAdd){
+				Toast.makeText(getApplicationContext(), "タイトルを入力してください", Toast.LENGTH_LONG).show();
+			}
+			else{
+				finish();
+			}
+			break;
+		}
 	}
 }

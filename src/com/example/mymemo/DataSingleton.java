1e415d051a47d701c58ad6a8b443d12db893958c
koji <yasuhirokojimakoyonplete@gmail.com>
2015-01-03 16:36:31 +0900

    データ管理クラスにデータ保持、データの取得、追加、削除処理の更新

--- src/com/example/mymemo/DataSingleton.java
+++ src/com/example/mymemo/DataSingleton.java
@@ -1,11 +1,29 @@
 package com.example.mymemo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
 public class DataSingleton {
 	private static DataSingleton instance = null;
-	private DataSingleton(){}
+	private DataSingleton(){
+		Init();
+		Log.i("abc","init");
+	}
 	//定数
 	public static final int DATATYPE_TITLE = 0;
 	public static final int DATATYPE_MAIN = 1;
+	
+	private List<String> title_list;
+	private List<String> main_list;
+	
+	private Context main_c;
 	
 	public static DataSingleton getInstance(){
 		if(instance == null){
@@ -13,17 +31,55 @@ public class DataSingleton {
 		}
 		return instance;
 	}
+	public void Init(){
+		//初期のみ
+		title_list=new ArrayList<String>(Arrays.asList("太陽","月","星","節制"));
+		main_list=new ArrayList<String>(Arrays.asList("それはまぶしすぎる","疑心","たったひとつの真実を求めて","金"));
+		
+		//SharedPreferences save_pre = PreferenceManager.getDefaultSharedPreferences(main_c);
+		//save_pre.edit().putStringSet("title",title_list);
+	}
+	public void SetContext(Context c){
+		main_c = c;
+	}
 	public String[] getData(int type){
 		String[] data = null;
+		int index = 0;
 		switch(type){
 		case DATATYPE_TITLE:
-			data = new String[]{"太陽","月","星"};
+			//data = new String[]{"太陽","月","星"};
+			data = new String[title_list.size()];
+			for (String str_data : title_list){
+				data[index] = str_data;
+				index++;
+			}
 			break;
 		case DATATYPE_MAIN:
-			data = new String[]{"それはまぶしすぎる","疑心","たったひとつの真実を求めて"};
+			//data = new String[]{"それはまぶしすぎる","疑心","たったひとつの真実を求めて","金"};
+			data = new String[main_list.size()];
+			for (String str_data : main_list){
+				data[index] = str_data;
+				index++;
+			}
 			break;
 		
 		}
 	     return data;
+	}
+	public boolean AddData(String title_str,String main_str){
+		if(title_str.equals("")){
+			return false;
+		}
+		else{
+			title_list.add(title_str);
+			main_list.add(main_str);
+			return true;
+		}
+		//Log.i(title_str,main_str);
+		//Log.i(title_str,title_list.get(4));
+	}
+	public void DeleteData(int position){
+		title_list.remove(position);
+		main_list.remove(position);
 	}
 }

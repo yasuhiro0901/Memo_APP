7a2ff5466763c564f70789a3a8ec554f128b5b86
koji <yasuhirokojimakoyonplete@gmail.com>
2015-01-03 16:42:33 +0900

    ListViewのリストを作成するタイミングを変更。画面遷移の時も行えるようにするため。

--- src/com/example/mymemo/MainActivity.java
+++ src/com/example/mymemo/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Adapter;
 //import android.view.Menu;
 //import android.view.MenuItem;
 //import android.widget.ArrayAdapter;
@@ -37,38 +38,64 @@ public class MainActivity extends Activity implements OnClickListener{
         add_button.setOnClickListener(this); 
        
         dataS = DataSingleton.getInstance();
-        titleText = dataS.getData(DataSingleton.DATATYPE_TITLE);
-        mainText = dataS.getData(DataSingleton.DATATYPE_MAIN);
-        
-        List<Map<String,String>> list = new ArrayList<Map<String,String>>();
-        for(int i=0; i<titleText.length;i++){
-        	Map<String,String> map = new HashMap<String, String>();
-        	map.put("title", titleText[i]);
-        	map.put("main", mainText[i]);
-        	list.add(map);
-        }
-        
-        SimpleAdapter adapter = new SimpleAdapter(
-                this,
-                list,
-                android.R.layout.simple_list_item_2,
-                new String[]{"title","main"},
-                new int[] {android.R.id.text1,android.R.id.text2});
-            //adapter.add("listview item 1");
-            //adapter.add("listview item 2");
-            //adapter.add("listview item 3");
+//        titleText = dataS.getData(DataSingleton.DATATYPE_TITLE);
+//        mainText = dataS.getData(DataSingleton.DATATYPE_MAIN);
+//        
+//        List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+//        for(int i=0; i<titleText.length;i++){
+//        	Map<String,String> map = new HashMap<String, String>();
+//        	map.put("title", titleText[i]);
+//        	map.put("main", mainText[i]);
+//        	list.add(map);
+//        }
+//        
+//        adapter = new SimpleAdapter(
+//                this,
+//                list,
+//                android.R.layout.simple_list_item_2,
+//                new String[]{"title","main"},
+//                new int[] {android.R.id.text1,android.R.id.text2});
+//            //adapter.add("listview item 1");
+//            //adapter.add("listview item 2");
+//            //adapter.add("listview item 3");
 
             // リストビューにデータを設定
-            ListView listView1 = (ListView)findViewById(R.id.listView1);
-            listView1.setAdapter(adapter);
+            listView1 = (ListView)findViewById(R.id.listView1);
+           // listView1.setAdapter(adapter);
             
          listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         	 public void onItemClick(AdapterView<?> parent,View view,int pos,long id){
         		 ListView listView = (ListView) parent;
         		 Map<?, ?> item = (Map<?, ?>) listView.getItemAtPosition(pos);
-        		 Toast.makeText(getApplicationContext(), item.get("title").toString(), Toast.LENGTH_LONG).show();
+        		 Toast.makeText(getApplicationContext(), item.get("title").toString()+pos, Toast.LENGTH_LONG).show();
         	 }
-		});
+		});  
+    }
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	 titleText = dataS.getData(DataSingleton.DATATYPE_TITLE);
+         mainText = dataS.getData(DataSingleton.DATATYPE_MAIN);
+         
+   	  List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+       for(int i=0; i<titleText.length;i++){
+       	Map<String,String> map = new HashMap<String, String>();
+       	map.put("title", titleText[i]);
+       	map.put("main", mainText[i]);
+       	list.add(map);
+       }
+         
+ //   	adapter.notifyDataSetChanged();
+//    	Log.i("abc","start");
+
+//          
+          SimpleAdapter adapter = new SimpleAdapter(
+                  this,
+                  list,
+                  android.R.layout.simple_list_item_2,
+                  new String[]{"title","main"},
+                  new int[] {android.R.id.text1,android.R.id.text2});
+          listView1.setAdapter(adapter);
     }
 	@Override
 	public void onClick(View arg0) {

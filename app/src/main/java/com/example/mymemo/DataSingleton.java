4b2821d86f75fa77d84b7f8d6e97c3c323f91da4
koji <yasuhirokojimakoyonplete@gmail.com>
2015-12-31 01:31:48 +0900

    AndroidStudioでのビルド時にXML、java、画像が別の場所に生成されたため追加

---
+++ app/src/main/java/com/example/mymemo/DataSingleton.java
@@ -0,0 +1,157 @@
+package com.example.mymemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DataSingleton {
+	private static DataSingleton instance = null;
+	private DataSingleton(Context ct){
+		SetContext(ct);
+		Init();
+//		Log.i("abc","init");
+	}
+	//定数
+	public static final int DATATYPE_TITLE = 0;
+	public static final int DATATYPE_MAIN = 1;
+	
+	private List<String> title_list;
+	private List<String> main_list;
+	
+	private Context main_c;
+	
+	private int data_size;
+	
+	public static DataSingleton getInstance(Context c){
+		if(instance == null){
+			instance = new DataSingleton(c);
+		}
+		return instance;
+	}
+	public void Init(){
+		SharedPreferences save_pre = PreferenceManager.getDefaultSharedPreferences(main_c);
+		
+		if(!save_pre.getString("isfirst", "").equals("not")){
+			Log.i("abc","初回起動");
+			Log.i("abc","isfirst"+save_pre.getString("isfirst", ""));
+			//初期のみ
+			title_list=new ArrayList<String>(Arrays.asList("太陽","月","星","節制"));
+			main_list=new ArrayList<String>(Arrays.asList("それはまぶしすぎる","疑心","たったひとつの真実を求めて","金"));
+			
+			SaveData();
+			SetDataSize();
+			
+			Editor editor = save_pre.edit();
+			editor.putString("isfirst","not");
+			editor.apply();
+		}
+		else{
+			Log.i("abc","すでに起動してますね");
+			//data_size = 4;
+			if(GetDataSize() != -1){
+				title_list = new ArrayList<String>();
+				main_list = new ArrayList<String>();
+				for(int i = 0;i<GetDataSize();i++){
+					title_list.add(save_pre.getString("title"+i, ""));
+					main_list.add(save_pre.getString("main"+i, ""));
+				}
+			}
+			else {
+				Log.i("abc","取得できなかったので初回起動を行います");
+				Editor editor = save_pre.edit();
+				editor.putString("isfirst","first");
+				editor.apply();
+				Init();
+			}
+		}
+		
+	}
+	public void SetContext(Context c){
+		main_c = c;
+	}
+	public String[] getData(int type){
+		String[] data = null;
+		int index = 0;
+		switch(type){
+		case DATATYPE_TITLE:
+			//data = new String[]{"太陽","月","星"};
+			data = new String[title_list.size()];
+			for (String str_data : title_list){
+				data[index] = str_data;
+				index++;
+			}
+			break;
+		case DATATYPE_MAIN:
+			//data = new String[]{"それはまぶしすぎる","疑心","たったひとつの真実を求めて","金"};
+			data = new String[main_list.size()];
+			for (String str_data : main_list){
+				data[index] = str_data;
+				index++;
+			}
+			break;
+		
+		}
+	     return data;
+	}
+	public boolean AddData(String title_str,String main_str){
+		if(title_str.equals("")){
+			return false;
+		}
+		else{
+			title_list.add(title_str);
+			main_list.add(main_str);
+			return true;
+		}
+		//Log.i(title_str,main_str);
+		//Log.i(title_str,title_list.get(4));
+	}
+	public boolean EditData(String title_str,String main_str,int pos){
+		if(title_str.equals("")){
+			return false;
+		}
+		else{
+			title_list.set(pos,title_str);
+			main_list.set(pos, main_str);
+			return true;
+		}
+	}
+	public void DeleteData(int position){
+		title_list.remove(position);
+		main_list.remove(position);
+	}
+	
+	public void SetDataSize(){
+		data_size = title_list.size();
+		SharedPreferences save_pre = PreferenceManager.getDefaultSharedPreferences(main_c);
+		Editor editor = save_pre.edit();
+		editor.putInt("dataSize",data_size);
+		editor.apply();
+	}
+	public int GetDataSize(){
+		SharedPreferences save_pre = PreferenceManager.getDefaultSharedPreferences(main_c);
+		data_size = save_pre.getInt("dataSize", -1);
+		return data_size;
+	}
+	
+	public void SaveData(){
+		SharedPreferences save_pre = PreferenceManager.getDefaultSharedPreferences(main_c);
+		Editor editor = save_pre.edit();
+		for(int i = 0;i<title_list.size();i++){
+			editor.putString("title"+i, title_list.get(i));
+			Log.i("abc",title_list.get(i)+"を保存しました。title");
+		}
+		for(int i = 0;i<main_list.size();i++){
+			editor.putString("main"+i, main_list.get(i));
+			Log.i("abc",main_list.get(i)+"を保存しました。main");
+		}
+		SetDataSize();
+		editor.apply();
+	}
+}
---
+++ app/src/main/java/com/example/mymemo/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.mymemo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Adapter;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener{
+	private String[] titleText;
+	private String[] mainText;
+	private DataSingleton dataS;
+	private ListView listView1;
+	private SimpleAdapter adapter;
+	private HashMap<String,String> btn_option_data;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        ImageButton add_button = (ImageButton)findViewById(R.id.imageButton1);
+        add_button.setOnClickListener(this); 
+       
+        dataS = DataSingleton.getInstance(this);
+        
+        
+//        titleText = dataS.getData(DataSingleton.DATATYPE_TITLE);
+//        mainText = dataS.getData(DataSingleton.DATATYPE_MAIN);
+//        
+//        List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+//        for(int i=0; i<titleText.length;i++){
+//        	Map<String,String> map = new HashMap<String, String>();
+//        	map.put("title", titleText[i]);
+//        	map.put("main", mainText[i]);
+//        	list.add(map);
+//        }
+//        
+//        adapter = new SimpleAdapter(
+//                this,
+//                list,
+//                android.R.layout.simple_list_item_2,
+//                new String[]{"title","main"},
+//                new int[] {android.R.id.text1,android.R.id.text2});
+//            //adapter.add("listview item 1");
+//            //adapter.add("listview item 2");
+//            //adapter.add("listview item 3");
+
+            // リストビューにデータを設定
+            listView1 = (ListView)findViewById(R.id.listView1);
+           // listView1.setAdapter(adapter);
+            
+         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	 public void onItemClick(AdapterView<?> parent,View view,int pos,long id){
+        		 ListView listView = (ListView) parent;
+        		 Map<?, ?> item = (Map<?, ?>) listView.getItemAtPosition(pos);
+        		 Toast.makeText(getApplicationContext(), item.get("title").toString()+pos, Toast.LENGTH_SHORT).show();
+        		 
+        		 btn_option_data = new HashMap<String,String>();
+        			btn_option_data.put("doType", "edit");
+        			btn_option_data.put("title_str", item.get("title").toString());
+        			btn_option_data.put("main_str", item.get("main").toString());
+        			btn_option_data.put("position", String.valueOf(pos));
+        			
+        			Intent intent = new Intent(MainActivity.this,InputActivity.class);
+        			intent.putExtra("AddType", btn_option_data);
+        			startActivity(intent);
+        	 }
+		});  
+    }
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	 titleText = dataS.getData(DataSingleton.DATATYPE_TITLE);
+         mainText = dataS.getData(DataSingleton.DATATYPE_MAIN);
+         
+   	  List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+       for(int i=0; i<titleText.length;i++){
+       	Map<String,String> map = new HashMap<String, String>();
+       	map.put("title", titleText[i]);
+       	map.put("main", mainText[i]);
+       	list.add(map);
+       }
+         
+ //   	adapter.notifyDataSetChanged();
+//    	Log.i("abc","start");
+
+//          
+          SimpleAdapter adapter = new SimpleAdapter(
+                  this,
+                  list,
+                  android.R.layout.simple_list_item_2,
+                  new String[]{"title","main"},
+                  new int[] {android.R.id.text1,android.R.id.text2});
+          listView1.setAdapter(adapter);
+    }
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(getApplicationContext(), "＋押されました", Toast.LENGTH_LONG).show();
+		
+		btn_option_data = new HashMap<String,String>();
+		btn_option_data.put("doType", "new");
+		btn_option_data.put("title_str", "");
+		btn_option_data.put("main_str", "");
+		
+		Intent intent = new Intent(MainActivity.this,InputActivity.class);
+		intent.putExtra("AddType", btn_option_data);
+		startActivity(intent);
+	}
+}
